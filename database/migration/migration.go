f15f60bf26c29350b9571e7458af1c169625b7b8
adyfp24 <adyfp24@gmail.com>
2024-02-05 23:12:04 +0700

    feat(mihgration):  create and running migration for user table

--- database/migration/migration.go
+++ database/migration/migration.go
@@ -1,1 +1,17 @@
 package migration
+
+import(
+	"fmt"
+	"github.com/adyfp24/golang-jwt-auth/database"
+	"github.com/adyfp24/golang-jwt-auth/models"
+	"log"
+)
+
+func RunMigration(){
+	db, err := database.InitDB()
+	err = db.AutoMigrate(&models.User{})
+	if err != nil{
+		log.Print(err)
+	}
+	fmt.Println("migrasi berhasil dijalankan")
+}
--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"log"
+
+	"github.com/adyfp24/golang-jwt-auth/database/migration"
+	"github.com/adyfp24/golang-jwt-auth/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/adyfp24/golang-jwt-auth/routes"
-	"github.com/adyfp24/golang-jwt-auth/database"
 )
 
 func main(){
-	database.InitDB()
+	migration.RunMigration()
+
 	app := fiber.New()
 	app.Use(cors.New())
+
 	routes.RouteInit(app)
+	
 	err := app.Listen(":4000")
 	if err != nil{
 		log.Fatal(err)
